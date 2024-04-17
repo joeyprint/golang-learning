59da5850429970dc5296b4f3e3945f09d986fe81
Joey Wasin <wasin.ww1402@gmail.com>
2024-04-17 12:00:34 +0700

    feat: change name to router

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
